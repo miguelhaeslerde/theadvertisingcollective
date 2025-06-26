cadefcfe3af106d0cb97db31a42cb0377bdb408b
Miguel Haesler <kontakt@miguelhaesler.de>
2025-06-26 11:35:38 +0000

    Fix import path for Toaster component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { HelmetProvider } from 'react-helmet-async';
-import { Toaster } from "@/components/ui/toaster";
+import { HelmetProvider } from "react-helmet-async";
+import { Toaster } from "./components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider } from "@/contexts/ThemeContext";
 import MaintenanceOverlay from "@/components/MaintenanceOverlay";
